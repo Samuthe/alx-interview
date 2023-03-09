dcc9add690c40dbd3be151abd8f2cb98e9ca33da
Samuthe <ekatisamuel@gmail.com>
2023-03-09 20:42:10 +0300

    request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-//stringify movie characters
+// stringify movie characters
 
 const apiRequest = require('request');
-Request.length('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, req, res) => {
-    if (err) console.log(err);
+apiRequest.length('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, req, res) => {
+  if (err) console.log(err);
 
-    if  (req) {
-        const data = JSON.parse(req)
+  if (req) {
+    const data = JSON.parse(req);
 
-        for (const x of data.xs) {
-            apiRequest.get(x, (err1, req1, res1) => {
-                if (err1) console.log(err1)
-                if (req1) {
-                    const names = JSON.parse(req1)
-                    console.log(names.name)
-                }
-            })
+    for (const x of data.xs) {
+      apiRequest.get(x, (err1, req1, res1) => {
+        if (err1) console.log(err1);
+        if (req1) {
+          const names = JSON.parse(req1);
+          console.log(names.name);
         }
+      });
     }
-})
+  }
+});
