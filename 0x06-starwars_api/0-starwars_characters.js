1142aef05286ee585e8fdaf03c4f02e4e97062c9
Samuthe <ekatisamuel@gmail.com>
2023-03-10 10:35:05 +0300

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,42 +4,29 @@
 const request = require('request');
 const arg = process.argv[2];
 
-async function resUrl(dataUrl) {
-  return new Promise(function(resolve, reject) {
-    request(dataUrl, function (err, res, body){
+async function resUrl (dataUrl) {
+  return new Promise(function (resolve, reject) {
+    request(dataUrl, function (err, res, body) {
       resolve(JSON.parse(body).name);
-      if(err) throw err;
+      if (err) throw err;
     });
   });
 }
 
-async function chars() {
-  return new Promise(function(resolve, reject){
-    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, function(err, res, bod){
+async function chars () {
+  return new Promise(function (resolve, reject) {
+    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, function (err, res, bod) {
       resolve(JSON.parse(bod).characters);
-      if(err) throw err;
+      if (err) throw err;
     });
   });
 }
 
-async function names() {
+async function names () {
   const mychars = await chars();
-  for(let i = 0; i < mychars.length; i++){
-    console.log(await resUrl(mychars[i]))
+  for (let i = 0; i < mychars.length; i++) {
+    console.log(await resUrl(mychars[i]));
   }
 }
 
 names();
-
-
-
-
-
-
-
-
-
-
-
-
-
