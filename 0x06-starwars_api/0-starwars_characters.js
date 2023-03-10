0f2ec78814a4bc232edcd1070fd13044dc67baf0
Samuthe <ekatisamuel@gmail.com>
2023-03-10 09:36:08 +0300

    request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,8 +8,8 @@ apiRequest.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (
   if (req) {
     const data = JSON.parse(res);
 
-    for (const x of data.xs) {
-      apiRequest.get(x, (err1, req1, res1) => {
+    for (const character of data.characters) {
+      apiRequest.get(character, (err1, req1, res1) => {
         if (err1) console.log(err1);
         if (req1) {
           const names = JSON.parse(res1);
