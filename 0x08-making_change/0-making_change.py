243db41c0f0f9deb11f28672eefa4b3dfa18c6fc
Samuthe <ekatisamuel@gmail.com>
2023-03-21 21:14:55 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,7 +9,7 @@ def makeChange(coins, total):
     Given a pile of coins of different values,
     determine the fewest number of coins needed
     to meet a given amount total.
-    Dyanimc Programmming Bottom Up Solution
+    Dynamic Programmming Bottom Up Solution
     """
     if total <= 0:
         return 0
