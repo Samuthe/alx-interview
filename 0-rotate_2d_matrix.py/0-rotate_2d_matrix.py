c06bb444f05e6590727b2c5374353ff85d9278b1
Samuthe <ekatisamuel@gmail.com>
2023-03-22 17:10:33 +0300

    matrix

--- 0-rotate_2d_matrix.py/0-rotate_2d_matrix.py
+++ 0-rotate_2d_matrix.py/0-rotate_2d_matrix.py
@@ -1,27 +1,3 @@
-# #!/usr/bin/python3
-
-# '''rotate matrix 90 degrees
-# '''
-
-
-# def rotate_2d_matrix(matrix):
-#     '''given n x n, rotate 2D matrix 90 degrees clockwise
-#     '''
-#     n = len(matrix)
-
-#     for i in range(n):
-#         for j in range(i, n):
-#             temp = matrix[i][j]
-#             matrix[i][j] = matrix[j][i]
-#             matrix[j][i] = temp
-
-#     for i in range(n):
-#         for j in range(n // 2):
-#             temp = matrix[i][j]
-#             matrix[j][i] = matrix[i][n - 1 - j]
-#             matrix[n - i - j] = temp
-
-
 #!/usr/bin/python3
 """
 Module 0-rotate_2d_matrix
@@ -45,3 +21,25 @@ def rotate_2d_matrix(matrix):
             temp = matrix[i][j]
             matrix[i][j] = matrix[i][N - 1 - j]
             matrix[i][N - 1 - j] = temp
+
+
+# '''rotate matrix 90 degrees
+# '''
+
+
+# def rotate_2d_matrix(matrix):
+#     '''given n x n, rotate 2D matrix 90 degrees clockwise
+#     '''
+#     n = len(matrix)
+
+#     for i in range(n):
+#         for j in range(i, n):
+#             temp = matrix[i][j]
+#             matrix[i][j] = matrix[j][i]
+#             matrix[j][i] = temp
+
+#     for i in range(n):
+#         for j in range(n // 2):
+#             temp = matrix[i][j]
+#             matrix[j][i] = matrix[i][n - 1 - j]
+#             matrix[n - i - j] = temp
